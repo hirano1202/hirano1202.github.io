a4c1e330c130683c34708dd02e0b6dc49805d069
Hirano <95395772+hirano1202@users.noreply.github.com>
2022-02-21 14:32:51 +0900

    02211431

---
+++ precache-manifest.1f6e4e9aa3a52e338f60e911ce8636e1.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a0601e66daa9e73e576b1717baed98",
+    "url": "app-touch-icon.png"
+  },
+  {
+    "revision": "8f4c3bc01d08fcbec052",
+    "url": "css/app.30d06776.css"
+  },
+  {
+    "revision": "8f4c3bc01d08fcbec052",
+    "url": "css/app.30d06776.css.map"
+  },
+  {
+    "revision": "874eb8d9acbecdafa38e",
+    "url": "css/chunk-17ca07b9.83f55ae2.css"
+  },
+  {
+    "revision": "874eb8d9acbecdafa38e",
+    "url": "css/chunk-17ca07b9.83f55ae2.css.map"
+  },
+  {
+    "revision": "6ab57f8176a0150b6972",
+    "url": "css/chunk-29822d6e.dc4da3fd.css"
+  },
+  {
+    "revision": "6ab57f8176a0150b6972",
+    "url": "css/chunk-29822d6e.dc4da3fd.css.map"
+  },
+  {
+    "revision": "61acefd70bf4a0978961",
+    "url": "css/chunk-2d6f34e4.bb71213d.css"
+  },
+  {
+    "revision": "61acefd70bf4a0978961",
+    "url": "css/chunk-2d6f34e4.bb71213d.css.map"
+  },
+  {
+    "revision": "de2551c22e79b127ca5d",
+    "url": "css/chunk-347f1218.9c3e71da.css"
+  },
+  {
+    "revision": "de2551c22e79b127ca5d",
+    "url": "css/chunk-347f1218.9c3e71da.css.map"
+  },
+  {
+    "revision": "6c16b754b2b1ce461006",
+    "url": "css/chunk-525c75df.10f17dfd.css"
+  },
+  {
+    "revision": "6c16b754b2b1ce461006",
+    "url": "css/chunk-525c75df.10f17dfd.css.map"
+  },
+  {
+    "revision": "89b30a9b4876150defa7",
+    "url": "css/chunk-efd2b29a.bbe85bc8.css"
+  },
+  {
+    "revision": "89b30a9b4876150defa7",
+    "url": "css/chunk-efd2b29a.bbe85bc8.css.map"
+  },
+  {
+    "revision": "55a1437ca473c1e6e6463b283e8b530e",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "bf1692b5b2dcc47d3006cc6a035faed7",
+    "url": "img/ProfImg.bf1692b5.png"
+  },
+  {
+    "revision": "0e49543e5738db2681114b8346dcd4bb",
+    "url": "img/YoruBg.0e49543e.png"
+  },
+  {
+    "revision": "ad9ac391a35f4fef21487bab9dff1752",
+    "url": "img/Yoru_Group.ad9ac391.png"
+  },
+  {
+    "revision": "a34064994d2b97104fd7703319435429",
+    "url": "img/computer.a3406499.png"
+  },
+  {
+    "revision": "5e182a0be58f422494cbcfdb4b3e914e",
+    "url": "img/howtouse_mobile.5e182a0b.gif"
+  },
+  {
+    "revision": "5b50c0448a930bd1e933c642680e3f01",
+    "url": "img/howtouse_pc.5b50c044.gif"
+  },
+  {
+    "revision": "0a2970f9a4d5ea6337c5074731ee90f8",
+    "url": "img/icons/android-chrome-128x128.png"
+  },
+  {
+    "revision": "ea54d68b6f9613e5fe8ce97710c628e7",
+    "url": "img/icons/android-chrome-144x144.png"
+  },
+  {
+    "revision": "53b9c8fe0d07a8d0a40a3e97e4ea18c1",
+    "url": "img/icons/android-chrome-152x152.png"
+  },
+  {
+    "revision": "bdd327e8803cb3942e4651771bf52dbb",
+    "url": "img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "8adfc2c071cffeb034ae49a13f148136",
+    "url": "img/icons/android-chrome-256x256.png"
+  },
+  {
+    "revision": "232cb860bdc0865d61c5f23a8b4832f8",
+    "url": "img/icons/android-chrome-36x36.png"
+  },
+  {
+    "revision": "a7d45bd8223bcb519bde764bc790a161",
+    "url": "img/icons/android-chrome-384x384.png"
+  },
+  {
+    "revision": "67628a4399c7bd097a15fc450b6b9dcd",
+    "url": "img/icons/android-chrome-48x48.png"
+  },
+  {
+    "revision": "09dcc9590148a56e447dad8a8358c82c",
+    "url": "img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "57809baaa901d3b6fc37446a26d87dc9",
+    "url": "img/icons/android-chrome-72x72.png"
+  },
+  {
+    "revision": "e38b0309d310fc6aa2ee009c32a1cb4a",
+    "url": "img/icons/android-chrome-96x96.png"
+  },
+  {
+    "revision": "cca006914f745f9bd2f9236b8f3f9762",
+    "url": "img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "bdd327e8803cb3942e4651771bf52dbb",
+    "url": "img/icons/icon-192x192.png"
+  },
+  {
+    "revision": "4e13d00f21b895c61e8860afe5c64166",
+    "url": "img/logo.4e13d00f.svg"
+  },
+  {
+    "revision": "998f9f985b7b945b638fc0eeb2b2b1a6",
+    "url": "img/logo2.998f9f98.svg"
+  },
+  {
+    "revision": "a5fde412860d90d438c1684cb48e2522",
+    "url": "img/smartphone.a5fde412.png"
+  },
+  {
+    "revision": "af9086072be63ffed0360f058ffcbebe",
+    "url": "index.html"
+  },
+  {
+    "revision": "8f4c3bc01d08fcbec052",
+    "url": "js/app.fef66020.js"
+  },
+  {
+    "revision": "874eb8d9acbecdafa38e",
+    "url": "js/chunk-17ca07b9.456242fb.js"
+  },
+  {
+    "revision": "6ab57f8176a0150b6972",
+    "url": "js/chunk-29822d6e.ca7bc3e9.js"
+  },
+  {
+    "revision": "61acefd70bf4a0978961",
+    "url": "js/chunk-2d6f34e4.cd079be6.js"
+  },
+  {
+    "revision": "de2551c22e79b127ca5d",
+    "url": "js/chunk-347f1218.3529dec7.js"
+  },
+  {
+    "revision": "6c16b754b2b1ce461006",
+    "url": "js/chunk-525c75df.b48faabc.js"
+  },
+  {
+    "revision": "89b30a9b4876150defa7",
+    "url": "js/chunk-efd2b29a.ceed9384.js"
+  },
+  {
+    "revision": "4ab260f7ece1f74060a19ec70971552d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b0632fbe713e6b002a4d0c30e33ac8e1",
+    "url": "media/howtouse_pc.b0632fbe.webm"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.1f6e4e9aa3a52e338f60e911ce8636e1.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "summary_app"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
